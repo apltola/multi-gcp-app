43d8253d5d97ef7434c77ba74bdb8ace6e1a730c
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2023-05-18 21:28:38 +0300

    dockerizeeeee

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -24,5 +24,5 @@ app.use(errorHandler);
 const port = process.env.PORT || 5555;
 
 app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
+  console.log(`Api is listening on ${port}`);
 });
