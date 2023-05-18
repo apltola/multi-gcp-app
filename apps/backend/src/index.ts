08c2af2d4842f7c4c4afd2112e16d2ce1ad47c3b
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2023-05-18 21:06:21 +0300

    add cors and health check

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import candidateRouter from './routes/candidates/router';
 import jobsRouter from './routes/jobs/router';
+import healthCheckRouter from './routes/health/router';
 import logRequest from './middleware/logRequest';
 import notFoundHandler from './handlers/notFound';
 import errorHandler from './handlers/error';
@@ -9,14 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use(logRequest);
 app.use('/candidates', candidateRouter);
 app.use('/jobs', jobsRouter);
-
-app.get('/', (req, res) => {
-  res.send('server is alive');
-});
+app.use('/', healthCheckRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
